693990ecf918fbb3b8cac5948b12715b49981de0
wenjie88 <350668770@qq.com>
2017-09-08 17:58:40 +0800

    add

---
+++ Model/UserInfo.cs
@@ -0,0 +1,33 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace weixin.Model
+{
+    public class UserInfo
+    {
+        //用户是否订阅该公众号标识，值为0时，代表此用户没有关注该公众号，拉取不到其余信息。
+        public string subscribe { get; set; }
+        public string openid { get; set; }
+        public string nickname { get; set; }
+        public string language { get; set; }
+
+        //用户的性别，值为1时是男性，值为2时是女性，值为0时是未知
+        public int sex { get; set; }
+        public string province { get; set; }
+        public string city { get; set; }
+        public string country { get; set; }
+        public string headimgurl { get; set; }
+        public decimal subscribe_time { get; set; }
+        public string remark { get; set; }
+
+        //只有在用户将公众号绑定到微信开放平台帐号后，才会出现该字段。
+        public string unionid { get; set; }
+
+        //错误
+        public string errcode { get; set; }
+        public string errmsg { get; set; }
+    }
+}
---
+++ WxUser.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using weixin.Model;
+using weixin.Utils;
+
+namespace weixin
+{
+    public class WxUser
+    {
+        public static UserInfo GetWxUserInfo(string OpenId)
+        {
+            string access_token = Wx.GetAccessToken();
+            string url = $"https://api.weixin.qq.com/cgi-bin/user/info?access_token={access_token}&openid={OpenId}&lang=zh_CN";
+            string responeStr = HttpRequsetHelper.Get(url);
+            UserInfo res = Newtonsoft.Json.JsonConvert.DeserializeObject<UserInfo>(responeStr);
+            return res;
+        }
+        
+    }
+}
